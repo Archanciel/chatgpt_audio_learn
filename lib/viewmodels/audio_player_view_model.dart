1a753e71195982f4e551e203ce821ea7229712aa
Archanciel <jp.schnyder@gmail.com>
2023-02-25 15:49:31 +0100

    remoWorks.ved no longer useful code.

--- lib/viewmodels/audio_player_view_model.dart
+++ lib/viewmodels/audio_player_view_model.dart
@@ -4,38 +4,23 @@ import 'package:audioplayer/audioplayer.dart';
 
 import '../models/audio.dart';
 
-enum AudioPlayerState {
-  playing,
-  paused,
-  stopped,
-}
-
 class AudioPlayerViewModel extends ChangeNotifier {
-  AudioPlayerState _state = AudioPlayerState.stopped;
-  AudioPlayerState get state => _state;
-
   final AudioPlayer _audioPlayer = AudioPlayer();
 
-  Future<AudioPlayerState> play(Audio audio) async {
+  Future<void> play(Audio audio) async {
     final file = File(audio.filePath);
     if (!await file.exists()) {
       print('File not found: ${audio.filePath}');
-      return AudioPlayerState.stopped;
     }
 
     _audioPlayer.stop();
     await _audioPlayer.play(audio.filePath);
     audio.isPlaying = true;
 
-
-
-    // Play the audio using the provided filePath
-    _state = AudioPlayerState.playing;
     notifyListeners();
-    return _state;
   }
 
-  Future<AudioPlayerState> pause(Audio audio) async {
+  Future<void> pause(Audio audio) async {
     // Stop the audio
       if (audio.isPlaying) {
       await _audioPlayer.pause();
@@ -45,17 +30,13 @@ class AudioPlayerViewModel extends ChangeNotifier {
 
     audio.invertPlaying();
 
-    _state = AudioPlayerState.paused;
     notifyListeners();
-    return _state;
   }
 
-  Future<AudioPlayerState> stop(Audio audio) async {
+  Future<void> stop(Audio audio) async {
     // Stop the audio
-    _state = AudioPlayerState.stopped;
     await _audioPlayer.stop();
     audio.isPlaying = false;
     notifyListeners();
-    return _state;
   }
 }
