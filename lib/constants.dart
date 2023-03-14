8ebfaf867a0ea685d4a58b29242c9d4752cf09d6
Archanciel <jp.schnyder@gmail.com>
2023-03-14 21:50:32 +0100

    works on S20

--- lib/constants.dart
+++ lib/constants.dart
@@ -27,9 +27,9 @@ DateFormat frenchDateTimeFormat = DateFormat("dd-MM-yyyy HH:mm");
 
 const bool kDeleteAppDir = false;
 // to_delete playlist url
-const String kPlaylistUrl =
-    'https://youtube.com/playlist?list=PLzwWSJNcZTMTB9iwbu77FGokc3WsoxuV0';
+// const String kPlaylistUrl =
+//     'https://youtube.com/playlist?list=PLzwWSJNcZTMTB9iwbu77FGokc3WsoxuV0';
 
 // audio_learn playlist url
-// const String kPlaylistUrl =
-//     'https://youtube.com/playlist?list=PLzwWSJNcZTMQL091OKgsyHnyvjSajDVpC';
+const String kPlaylistUrl =
+    'https://youtube.com/playlist?list=PLzwWSJNcZTMQL091OKgsyHnyvjSajDVpC';
