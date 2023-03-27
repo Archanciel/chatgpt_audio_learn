6a8b727e58ebff7062943e677c153ef5f98501cd
Archanciel <jp.schnyder@gmail.com>
2023-03-27 08:51:24 +0200

    Update constants.dart

--- lib/constants.dart
+++ lib/constants.dart
@@ -27,17 +27,20 @@ DateFormat frenchDateTimeFormat = DateFormat("dd-MM-yyyy HH:mm");
 
 const bool kDeleteAppDir = false;
 // to_delete playlist url
-// const String kPlaylistUrl =
-//     'https://youtube.com/playlist?list=PLzwWSJNcZTMTB9iwbu77FGokc3WsoxuV0';
-// const String kUniquePlaylistTitle = 'to_delete';
+const String kPlaylistUrl =
+    'https://youtube.com/playlist?list=PLzwWSJNcZTMTB9iwbu77FGokc3WsoxuV0';
+const String kUniquePlaylistTitle = 'to_delete';
 
 // audio_learn playlist url
-const String kPlaylistUrl =
-    'https://youtube.com/playlist?list=PLzwWSJNcZTMRBJuvE6vk6PMnqIBhCKUIa';
-const String kUniquePlaylistTitle = 'audio_learn';
+// const String kPlaylistUrl =
+//     'https://youtube.com/playlist?list=PLzwWSJNcZTMRBJuvE6vk6PMnqIBhCKUIa';
+// const String kUniquePlaylistTitle = 'audio_learn';
 
 const double kAudioDefaultSpeed = 1.25;
 const String kSecretClientCodeJsonFileName =
     'code_secret_client_923487935936-po8d733kjvrnee3l3ik3r5mebe8ebhr7.apps.googleusercontent.com.json';
 
 const kGoogleApiKey = 'AIzaSyDhywmh5EKopsNsaszzMkLJ719aQa2NHBw';
+
+/// A constant that is true if the application was compiled to run on the web.
+const bool kIsWeb = bool.fromEnvironment('dart.library.js_util');
