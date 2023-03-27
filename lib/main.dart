e05ed5dd4c85fb30f1dd14ac0e35f5c31b42182c
Archanciel <jp.schnyder@gmail.com>
2023-03-27 08:50:01 +0200

    improved dark theme

--- lib/main.dart
+++ lib/main.dart
@@ -64,8 +64,56 @@ class MyApp extends StatelessWidget {
         builder: (context, themeProvider, child) {
           return MaterialApp(
             title: 'Youtube Audio Downloader',
-            theme:
-                themeProvider.isDarkMode ? ThemeData.dark() : ThemeData.light(),
+            theme: themeProvider.isDarkMode
+                ? ThemeData.dark().copyWith(
+                    colorScheme: ThemeData.dark().colorScheme.copyWith(
+                          background: Colors.black,
+                          surface: Colors.black,
+                        ),
+                    primaryColor: Colors.black,
+                    scaffoldBackgroundColor: Colors.black,
+                    iconTheme: ThemeData.dark().iconTheme.copyWith(
+                          color: Colors
+                              .blue.shade700, // Set icon color in dark mode
+                        ),
+                    elevatedButtonTheme: ElevatedButtonThemeData(
+                      style: ElevatedButton.styleFrom(
+                        primary: Colors
+                            .blue.shade700, // Set button color in dark mode
+                        onPrimary:
+                            Colors.white, // Set button text color in dark mode
+                      ),
+                    ),
+                    textTheme: ThemeData.dark().textTheme.copyWith(
+                          bodyText2: ThemeData.dark()
+                              .textTheme
+                              .bodyText2!
+                              .copyWith(color: Colors.blue.shade700),
+                          subtitle1: ThemeData.dark()
+                              .textTheme
+                              .subtitle1!
+                              .copyWith(color: Colors.white),
+                        ),
+                    checkboxTheme: ThemeData.dark().checkboxTheme.copyWith(
+                          checkColor: MaterialStateProperty.all(
+                            Colors.blue.shade700, // Set Checkbox check color
+                          ),
+                          fillColor: MaterialStateProperty.all(
+                            Colors.white, // Set Checkbox fill color
+                          ),
+                        ),
+                    inputDecorationTheme: InputDecorationTheme(
+                      fillColor: Colors.grey[900],
+                      filled: true,
+                      border: OutlineInputBorder(),
+                    ),
+                    textSelectionTheme: TextSelectionThemeData(
+                      cursorColor: Colors.white,
+                      selectionColor: Colors.white.withOpacity(0.3),
+                      selectionHandleColor: Colors.white.withOpacity(0.5),
+                    ),
+                  )
+                : ThemeData.light(),
             home: Scaffold(
               appBar: AppBar(
                 title: const Text('Youtube Audio Downloader'),
