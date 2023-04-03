c3c0829efbb971f4d5f8cc7d5e3f3e00b26d3357
Archanciel <jp.schnyder@gmail.com>
2023-04-03 02:31:36 +0200

    Update constants.dart

--- lib/constants.dart
+++ lib/constants.dart
@@ -21,16 +21,16 @@ DateFormat englishDateTimeFormat = DateFormat("yyyy-MM-dd HH:mm");
 DateFormat frenchDateTimeFormat = DateFormat("dd-MM-yyyy HH:mm");
 
 // to_delete playlist url
-// const String kPlaylistUrl =
-//     'https://youtube.com/playlist?list=PLzwWSJNcZTMTB9iwbu77FGokc3WsoxuV0';
-// const String kUniquePlaylistTitle = 'to_delete';
-// const bool kDeleteAppDir = false;
+const String kPlaylistUrl =
+    'https://youtube.com/playlist?list=PLzwWSJNcZTMTB9iwbu77FGokc3WsoxuV0';
+const String kUniquePlaylistTitle = 'to_delete';
+const bool kDeleteAppDir = false;
 
 // audio_learn playlist url
-const String kPlaylistUrl =
-    'https://youtube.com/playlist?list=PLzwWSJNcZTMRBJuvE6vk6PMnqIBhCKUIa';
-const String kUniquePlaylistTitle = 'audio_learn';
-const bool kDeleteAppDir = false;
+// const String kPlaylistUrl =
+//     'https://youtube.com/playlist?list=PLzwWSJNcZTMRBJuvE6vk6PMnqIBhCKUIa';
+// const String kUniquePlaylistTitle = 'audio_learn';
+// const bool kDeleteAppDir = false;
 
 const double kAudioDefaultSpeed = 1.25;
 const String kSecretClientCodeJsonFileName =
