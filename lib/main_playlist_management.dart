b3c9214508332ec554ef128479ad23ec77c3ce64
Archanciel <jp.schnyder@gmail.com>
2023-04-03 02:58:00 +0200

    get video added date works, but removing video fails !

---
+++ lib/main_playlist_management.dart
@@ -0,0 +1,47 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+const String apiKey = 'AIzaSyDhywmh5EKopsNsaszzMkLJ719aQa2NHBw';
+
+Future<void> main() async {
+  String playlistId = 'PLzwWSJNcZTMTB9iwbu77FGokc3WsoxuV0';
+  String videoId = '4iRrusLxMIY';
+
+  // Obtain the date a video was added to a playlist
+  DateTime addedDate = await getVideoAddedDate(playlistId, videoId);
+  print('Video added on: $addedDate');
+
+  // Remove the video from the playlist
+  bool removed = await removeVideoFromPlaylist(playlistId, videoId);
+  print(removed ? 'Video removed successfully.' : 'Failed to remove the video.');
+}
+
+Future<DateTime> getVideoAddedDate(String playlistId, String videoId) async {
+  String url = 'https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=50&playlistId=$playlistId&videoId=$videoId&key=$apiKey';
+  var response = await http.get(Uri.parse(url));
+  var jsonResponse = jsonDecode(response.body);
+
+  if (jsonResponse['items'].length > 0) {
+    String publishedAt = jsonResponse['items'][0]['snippet']['publishedAt'];
+    return DateTime.parse(publishedAt);
+  } else {
+    throw Exception('Video not found in the playlist.');
+  }
+}
+
+Future<bool> removeVideoFromPlaylist(String playlistId, String videoId) async {
+  String url = 'https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=50&playlistId=$playlistId&videoId=$videoId&key=$apiKey';
+  var response = await http.get(Uri.parse(url));
+  var jsonResponse = jsonDecode(response.body);
+
+  if (jsonResponse['items'].length > 0) {
+    String playlistItemId = jsonResponse['items'][0]['id'];
+
+    String deleteUrl = 'https://www.googleapis.com/youtube/v3/playlistItems?id=$playlistItemId&key=$apiKey';
+    var deleteResponse = await http.delete(Uri.parse(deleteUrl));
+
+    return deleteResponse.statusCode == 204;
+  } else {
+    return false;
+  }
+}
