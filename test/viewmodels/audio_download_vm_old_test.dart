b5be0d0d29f023df4130c91ce1b8d8fd1cfc053b
Archanciel <jp.schnyder@gmail.com>
2023-04-03 20:40:59 +0200

    renamed file

---
+++ test/viewmodels/audio_download_vm_old_test.dart
@@ -0,0 +1,77 @@
+import 'package:audioplayers/audioplayers.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+import 'package:youtube_explode_dart/youtube_explode_dart.dart';
+
+import 'package:chatgpt_audio_learn/models/audio.dart';
+import 'package:chatgpt_audio_learn/viewmodels/audio_download_vm.dart';
+
+class MockYoutubeExplode extends Mock implements YoutubeExplode {}
+
+class MockPlaylist extends Mock implements Playlist {}
+
+class MockPlaylistVideo extends Mock implements Video {}
+
+class MockAudioStreamInfo extends Mock implements AudioStreamInfo {}
+
+void main() {
+  group('AudioViewModel', () {
+    late AudioDownloadVM viewModel;
+    late MockYoutubeExplode mockYoutubeExplode;
+
+    setUp(() {
+      mockYoutubeExplode = MockYoutubeExplode();
+      viewModel = AudioDownloadVM();
+      viewModel.youtubeExplode = mockYoutubeExplode;
+    });
+
+    test('fetchAudios downloads all audios', () async {
+      final mockPlaylist = MockPlaylist();
+      final mockVideo1 = MockPlaylistVideo();
+      final mockVideo2 = MockPlaylistVideo();
+      final mockAudioStreamInfo = MockAudioStreamInfo();
+
+      final videoTitle1 = 'Video 1';
+      final videoTitle2 = 'Video 2';
+      final videoDuration1 = Duration(minutes: 3, seconds: 42);
+      final videoDuration2 = Duration(minutes: 5, seconds: 21);
+
+      final expectedAudios = [
+        Audio(
+          originalVideoTitle: videoTitle1,
+          filePathName: '/storage/emulated/0/Download/Video 1.mp3',
+          audioPlayer: AudioPlayer(),
+          audioDuration: videoDuration1,
+        ),
+        Audio(
+          originalVideoTitle: videoTitle2,
+          filePathName: '/storage/emulated/0/Download/Video 2.mp3',
+          audioPlayer: AudioPlayer(),
+          audioDuration: videoDuration2,
+        )
+      ];
+
+      when(mockYoutubeExplode.playlists.get(any))
+          .thenAnswer((_) async => mockPlaylist);
+
+      when(mockPlaylist.videos).thenReturn([mockVideo1, mockVideo2]);
+
+      when(mockVideo1.title).thenReturn(videoTitle1);
+      when(mockVideo2.title).thenReturn(videoTitle2);
+
+      when(mockVideo1.duration).thenReturn(videoDuration1);
+      when(mockVideo2.duration).thenReturn(videoDuration2);
+
+      when(mockYoutubeExplode.videos.streamsClient.getManifest(any)).thenAnswer(
+          (_) async => Manifest(
+              audioOnly:
+                  StreamManifest(audioOnlyStreams: [mockAudioStreamInfo])));
+
+      when(mockAudioStreamInfo.url).thenReturn('http://example.com/audio.mp3');
+
+      await viewModel.downloadPlaylistAudios('http://example.com/playlist');
+
+      expect(viewModel.audioLst, expectedAudios);
+    });
+  });
+}
--- test/viewmodels/audio_download_vm_test.dart
+++ test/viewmodels/audio_download_vm_test.dart
@@ -1,77 +1,70 @@
-import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
-import 'package:youtube_explode_dart/youtube_explode_dart.dart';
 
-import 'package:chatgpt_audio_learn/models/audio.dart';
 import 'package:chatgpt_audio_learn/viewmodels/audio_download_vm.dart';
-
-class MockYoutubeExplode extends Mock implements YoutubeExplode {}
-
-class MockPlaylist extends Mock implements Playlist {}
-
-class MockPlaylistVideo extends Mock implements Video {}
-
-class MockAudioStreamInfo extends Mock implements AudioStreamInfo {}
+import 'package:youtube_explode_dart/youtube_explode_dart.dart' as yt;
+import '../mocks/mock_youtube_explode.dart';
+import '../mocks/mock_audio.dart';
 
 void main() {
-  group('AudioViewModel', () {
-    late AudioDownloadVM viewModel;
-    late MockYoutubeExplode mockYoutubeExplode;
+  group('AudioDownloadVM', () {
+    late AudioDownloadVM audioDownloadVM;
 
     setUp(() {
-      mockYoutubeExplode = MockYoutubeExplode();
-      viewModel = AudioDownloadVM();
-      viewModel.youtubeExplode = mockYoutubeExplode;
+      audioDownloadVM = AudioDownloadVM();
     });
 
-    test('fetchAudios downloads all audios', () async {
-      final mockPlaylist = MockPlaylist();
-      final mockVideo1 = MockPlaylistVideo();
-      final mockVideo2 = MockPlaylistVideo();
-      final mockAudioStreamInfo = MockAudioStreamInfo();
+    test('Check initial values', () {
+      expect(audioDownloadVM.listOfPlaylist, []);
+      expect(audioDownloadVM.isDownloading, false);
+      expect(audioDownloadVM.downloadProgress, 0.0);
+      expect(audioDownloadVM.lastSecondDownloadSpeed, 0);
+      expect(audioDownloadVM.isHighQuality, false);
+    });
 
-      final videoTitle1 = 'Video 1';
-      final videoTitle2 = 'Video 2';
-      final videoDuration1 = Duration(minutes: 3, seconds: 42);
-      final videoDuration2 = Duration(minutes: 5, seconds: 21);
+    test('Set audio quality', () {
+      audioDownloadVM.setAudioQuality(isHighQuality: true);
+      expect(audioDownloadVM.isHighQuality, true);
 
-      final expectedAudios = [
-        Audio(
-          originalVideoTitle: videoTitle1,
-          filePathName: '/storage/emulated/0/Download/Video 1.mp3',
-          audioPlayer: AudioPlayer(),
-          audioDuration: videoDuration1,
-        ),
-        Audio(
-          originalVideoTitle: videoTitle2,
-          filePathName: '/storage/emulated/0/Download/Video 2.mp3',
-          audioPlayer: AudioPlayer(),
-          audioDuration: videoDuration2,
-        )
-      ];
+      audioDownloadVM.setAudioQuality(isHighQuality: false);
+      expect(audioDownloadVM.isHighQuality, false);
+    });
 
+    test('downloadPlaylistAudios downloads audio files for a new playlist',
+        () async {
+      final mockYoutubeExplode = MockYoutubeExplode();
+      final mockYoutubePlaylist = MockYoutubePlaylist();
+      final mockYoutubeVideo = MockYoutubeVideo();
+      final mockStreamManifest = MockStreamManifest();
+      final mockAudioOnlyStreamInfo = MockAudioOnlyStreamInfo();
+
+      // Mocking calls to the YoutubeExplode library
       when(mockYoutubeExplode.playlists.get(any))
-          .thenAnswer((_) async => mockPlaylist);
+          .thenAnswer((_) => Future.value(mockYoutubePlaylist));
+      when(mockYoutubePlaylist.title).thenReturn('Mock Playlist');
+      when(mockYoutubeExplode.playlists.getVideos(any))
+          .thenAnswer((_) => Stream.value(mockYoutubeVideo));
+      when(mockYoutubeVideo.title).thenReturn('Mock Video');
+      when(mockYoutubeVideo.id).thenReturn(yt.VideoId('1234567890'));
+      when(mockYoutubeExplode.videos.streamsClient.getManifest(any))
+          .thenAnswer((_) => Future.value(mockStreamManifest));
+      when(mockStreamManifest.audioOnly.first)
+          .thenReturn(mockAudioOnlyStreamInfo);
+      when(mockStreamManifest.audioOnly.withHighestBitrate())
+          .thenReturn(mockAudioOnlyStreamInfo);
+      when(mockAudioOnlyStreamInfo.size)
+          .thenReturn(const yt.FileSize(1024 * 1024));
+      when(mockYoutubeExplode.videos.streamsClient.get(any!)).thenAnswer(
+          (_) => Stream.fromIterable([List<int>.filled(1024 * 1024, 0)]));
 
-      when(mockPlaylist.videos).thenReturn([mockVideo1, mockVideo2]);
+      final vm = AudioDownloadVM();
+      vm.youtubeExplode = mockYoutubeExplode;
 
-      when(mockVideo1.title).thenReturn(videoTitle1);
-      when(mockVideo2.title).thenReturn(videoTitle2);
+      final mockPlaylist = MockPlaylist();
+      await vm.downloadPlaylistAudios(playlistToDownload: mockPlaylist);
 
-      when(mockVideo1.duration).thenReturn(videoDuration1);
-      when(mockVideo2.duration).thenReturn(videoDuration2);
-
-      when(mockYoutubeExplode.videos.streamsClient.getManifest(any)).thenAnswer(
-          (_) async => Manifest(
-              audioOnly:
-                  StreamManifest(audioOnlyStreams: [mockAudioStreamInfo])));
-
-      when(mockAudioStreamInfo.url).thenReturn('http://example.com/audio.mp3');
-
-      await viewModel.downloadPlaylistAudios('http://example.com/playlist');
-
-      expect(viewModel.audioLst, expectedAudios);
+      expect(vm.listOfPlaylist.length, 1);
+      expect(vm.listOfPlaylist.first.downloadedAudioLst.length, 1);
     });
   });
 }
