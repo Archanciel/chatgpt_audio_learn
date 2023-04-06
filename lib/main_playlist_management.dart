ac46b7756c0c6c8fd729dd9d8d445b33c1c46759
Archanciel <jp.schnyder@gmail.com>
2023-04-07 00:25:12 +0200

    remove video not working

--- lib/main_playlist_management.dart
+++ lib/main_playlist_management.dart
@@ -6,6 +6,8 @@ const String apiKey = 'AIzaSyDhywmh5EKopsNsaszzMkLJ719aQa2NHBw';
 Future<void> main() async {
   String playlistId = 'PLzwWSJNcZTMTB9iwbu77FGokc3WsoxuV0';
   String videoId = '4iRrusLxMIY';
+  String videoIdLong = 'UEx6d1dTSk5jWlRNVEI5aXdidTc3Rkdva2MzV3NveHVWMC4zRjM0MkVCRTg0MkYyQTM0';
+  
 
   // Obtain the date a video was added to a playlist
   DateTime addedDate = await getVideoAddedDate(playlistId, videoId);
@@ -13,12 +15,16 @@ Future<void> main() async {
 
   // Remove the video from the playlist
   bool removed = await removeVideoFromPlaylist(playlistId, videoId);
-  print(removed ? 'Video removed successfully.' : 'Failed to remove the video.');
+  print(
+      removed ? 'Video removed successfully.' : 'Failed to remove the video.');
 }
 
 Future<DateTime> getVideoAddedDate(String playlistId, String videoId) async {
-  String url = 'https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=50&playlistId=$playlistId&videoId=$videoId&key=$apiKey';
-  var response = await http.get(Uri.parse(url));
+  String playlistUrl =
+      'https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=50&playlistId=$playlistId&key=$apiKey';
+  String videoUrl =
+      'https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=50&playlistId=$playlistId&videoId=$videoId&key=$apiKey';
+  var response = await http.get(Uri.parse(videoUrl));
   var jsonResponse = jsonDecode(response.body);
 
   if (jsonResponse['items'].length > 0) {
@@ -30,14 +36,16 @@ Future<DateTime> getVideoAddedDate(String playlistId, String videoId) async {
 }
 
 Future<bool> removeVideoFromPlaylist(String playlistId, String videoId) async {
-  String url = 'https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=50&playlistId=$playlistId&videoId=$videoId&key=$apiKey';
+  String url =
+      'https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=50&playlistId=$playlistId&videoId=$videoId&key=$apiKey';
   var response = await http.get(Uri.parse(url));
   var jsonResponse = jsonDecode(response.body);
 
   if (jsonResponse['items'].length > 0) {
     String playlistItemId = jsonResponse['items'][0]['id'];
 
-    String deleteUrl = 'https://www.googleapis.com/youtube/v3/playlistItems?id=$playlistItemId&key=$apiKey';
+    String deleteUrl =
+        'https://www.googleapis.com/youtube/v3/playlistItems?id=$playlistItemId&key=$apiKey';
     var deleteResponse = await http.delete(Uri.parse(deleteUrl));
 
     return deleteResponse.statusCode == 204;
