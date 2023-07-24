aa4eedb1215624c100e388be310fe8935e7310a7
Archanciel <jp.schnyder@gmail.com>
2023-07-24 19:41:58 +0200

    works

--- lib/constants.dart
+++ lib/constants.dart
@@ -61,6 +61,7 @@ DateFormat englishDateTimeFormat = DateFormat("yyyy-MM-dd HH:mm");
 DateFormat frenchDateTimeFormat = DateFormat("dd-MM-yyyy HH:mm");
 
 const double kRowWidthSeparator = 3.0;
+const double kRowHeightSeparator = 3.0;
 const double kSmallestButtonWidth = 40.0;
 const double kSmallButtonWidth = 48.0;
 const double kSmallButtonInsidePadding = 3.0;
--- lib/main_expandable_playlist.dart
+++ lib/main_expandable_playlist.dart
@@ -51,6 +51,15 @@ class ExpandablePlaylistListView extends StatefulWidget {
 class _ExpandablePlaylistListViewState
     extends State<ExpandablePlaylistListView> {
   final TextEditingController _playlistUrlController = TextEditingController();
+  final TextEditingController _smallTextFieldController =
+      TextEditingController();
+
+  @override
+  void dispose() {
+    _playlistUrlController.dispose();
+    _smallTextFieldController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -58,41 +67,82 @@ class _ExpandablePlaylistListViewState
       margin: const EdgeInsets.all(kDefaultMargin),
       child: Column(
         children: <Widget>[
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Expanded(
-                child: TextField(
-                  key: const Key('playlistUrlTextField'),
-                  controller: _playlistUrlController,
-                  decoration: const InputDecoration(
-                    labelText: 'Playlist URL)',
-                    border: OutlineInputBorder(),
+          SizedBox(
+            height: 50,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Expanded(
+                  // must exist to avoid exception
+                  child: Column(
+                    children: [
+                      Expanded(
+                        flex: 6, // controls the height ratio
+                        child: TextField(
+                          key: const Key('playlistUrlTextField'),
+                          maxLines: 1,
+                          style: const TextStyle(fontSize: 15),
+                          controller: _playlistUrlController,
+                          decoration: const InputDecoration(
+                            labelText: 'Playlist URL',
+                            hintText:
+                                'https://www.youtube.com/playlist?list=PL4cUxeGkcC9gjxLvV4VEkZ6H6H4yWuS58',
+                            border: OutlineInputBorder(),
+                            isDense: true,
+                            contentPadding: EdgeInsets.all(10),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(
+                        height:
+                            kRowHeightSeparator, // controls the space between TextFields
+                      ),
+                      Expanded(
+                        flex: 4, // controls the height ratio
+                        child: TextField(
+                          key: const Key('smallTextField'),
+                          maxLines: 1,
+                          style: const TextStyle(fontSize: 12),
+                          controller:
+                              _smallTextFieldController, // define a new controller for this field
+                          decoration: const InputDecoration(
+                            labelText: 'Small Field',
+                            hintText: 'Enter something here',
+                            border: OutlineInputBorder(),
+                            isDense: true,
+                            contentPadding: EdgeInsets.all(10),
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
                 ),
-              ),
-              const SizedBox(
-                width: kRowWidthSeparator,
-              ),
-              SizedBox(
-                width: kSmallButtonWidth,
-                child: ElevatedButton(
-                  key: const Key('addPlaylistButton'),
-                  style: ButtonStyle(
-                    shape: widget.appElevatedButtonRoundedShape,
-                    padding: MaterialStateProperty.all<EdgeInsetsGeometry>(
-                      const EdgeInsets.symmetric(
-                          horizontal: kSmallButtonInsidePadding),
+                const SizedBox(
+                  width: kRowWidthSeparator,
+                ),
+                SizedBox(
+                  width: kSmallButtonWidth,
+                  child: ElevatedButton(
+                    key: const Key('addPlaylistButton'),
+                    style: ButtonStyle(
+                      shape: widget.appElevatedButtonRoundedShape,
+                      padding: MaterialStateProperty.all<EdgeInsetsGeometry>(
+                        const EdgeInsets.symmetric(
+                            horizontal: kSmallButtonInsidePadding),
+                      ),
                     ),
+                    onPressed: () {},
+                    child: const Text('Add'),
                   ),
-                  onPressed: () {},
-                  child: const Text('Add'),
                 ),
-              ),
-              const SizedBox(
-                width: kRowWidthSeparator,
-              ),
-            ],
+                const SizedBox(
+                  width: kRowWidthSeparator,
+                ),
+              ],
+            ),
+          ),
+          SizedBox(
+            height: kRowHeightSeparator,
           ),
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
