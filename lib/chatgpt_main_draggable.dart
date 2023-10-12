8847de5914bebb8c35194368987f7c83b8b6b855
Archanciel <jp.schnyder@gmail.com>
2023-10-12 23:13:40 +0200

    black layout. Display icon no longer updated

--- lib/chatgpt_main_draggable.dart
+++ lib/chatgpt_main_draggable.dart
@@ -1,11 +1,62 @@
 import 'package:flutter/material.dart';
 
+const Color kIconColor = Color.fromARGB(246, 44, 61, 255);  // rgba(44, 61, 246, 255)
+const Color kButtonColor = Color(0xFF3D3EC2);
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData.dark().copyWith(
+        colorScheme: ThemeData.dark().colorScheme.copyWith(
+              background: Colors.black,
+              surface: Colors.black,
+            ),
+        primaryColor: Colors.black,
+        scaffoldBackgroundColor: Colors.black,
+        iconTheme: ThemeData.dark().iconTheme.copyWith(
+              color: kIconColor, // Set icon color in dark mode
+            ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: kButtonColor, // Set button color in dark mode
+            foregroundColor: Colors.white, // Set button text color in dark mode
+          ),
+        ),
+        textTheme: ThemeData.dark().textTheme.copyWith(
+              bodyMedium: ThemeData.dark()
+                  .textTheme
+                  .bodyMedium!
+                  .copyWith(color: kButtonColor),
+              titleMedium: ThemeData.dark()
+                  .textTheme
+                  .titleMedium!
+                  .copyWith(color: Colors.white),
+            ),
+        checkboxTheme: ThemeData.dark().checkboxTheme.copyWith(
+              checkColor: MaterialStateProperty.all(
+                Colors.white, // Set Checkbox fill color
+              ),
+              fillColor: MaterialStateProperty.all(
+                kIconColor, // Set Checkbox check color
+              ),
+            ),
+        // determines the background color and border of
+        // TextField
+        inputDecorationTheme: const InputDecorationTheme(
+          // fillColor: Colors.grey[900],
+          fillColor: Colors.black,
+          filled: true,
+          border: OutlineInputBorder(),
+        ),
+        textSelectionTheme: TextSelectionThemeData(
+          cursorColor: Colors.white,
+          selectionColor: Colors.white.withOpacity(0.3),
+          selectionHandleColor: Colors.white.withOpacity(0.5),
+        ),
+      ),
       home: MyHomePage(),
     );
   }
@@ -38,6 +89,7 @@ class _MyHomePageState extends State<MyHomePage> {
     return Scaffold(
       body: Column(
         children: [
+          SizedBox(height: 25),
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: _icons.asMap().entries.map((entry) {
