c75a432ba43c3e9e7f4e1d48f6db8494cd50d866
Archanciel <jp.schnyder@gmail.com>
2024-02-22 10:11:01 +0100

    work

---
+++ lib/main_provider_consumer.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+void main() => runApp(const MyApp());
+
+class Article {
+  final String id;
+  final String title;
+  bool isFavorite;
+
+  Article({required this.id, required this.title, this.isFavorite = false});
+}
+
+class FavoriteArticlesViewModel extends ChangeNotifier {
+  List<Article> _articles = [];
+
+  List<Article> get articles => _articles;
+
+  void addFavorite(String id) {
+    _articles.firstWhere((article) => article.id == id).isFavorite = true;
+    notifyListeners(); // Notifie les consommateurs d'un changement
+  }
+
+  void removeFavorite(String id) {
+    _articles.firstWhere((article) => article.id == id).isFavorite = false;
+    notifyListeners(); // Notifie les consommateurs d'un changement
+  }
+
+  // Initialisation avec des données d'exemple
+  void loadArticles() {
+    _articles = [
+      Article(id: "1", title: "Article 1"),
+      Article(id: "2", title: "Article 2"),
+    ];
+    notifyListeners();
+  }
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (context) {
+        FavoriteArticlesViewModel favoriteArticlesViewModel =
+            FavoriteArticlesViewModel();
+        favoriteArticlesViewModel.loadArticles();
+
+        return favoriteArticlesViewModel;
+      },
+      child: MaterialApp(
+        home: FavoriteArticlesScreen(),
+      ),
+    );
+  }
+}
+
+class FavoriteArticlesScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Articles Favoris"),
+      ),
+      body: Consumer<FavoriteArticlesViewModel>(
+        builder: (context, viewModel, child) {
+          return ListView.builder(
+            itemCount: viewModel.articles.length,
+            itemBuilder: (context, index) {
+              var article = viewModel.articles[index];
+              return ListTile(
+                title: Text(article.title),
+                trailing: Icon(
+                  article.isFavorite ? Icons.favorite : Icons.favorite_border,
+                  color: article.isFavorite ? Colors.red : null,
+                ),
+                onTap: () {
+                  article.isFavorite
+                      ? viewModel.removeFavorite(article.id)
+                      : viewModel.addFavorite(article.id);
+                },
+              );
+            },
+          );
+        },
+      ),
+    );
+  }
+}
---
+++ lib/main_provider_listen_true.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+void main() => runApp(const MyApp());
+
+class CounterViewModel extends ChangeNotifier {
+  int _count = 0;
+
+  int get count => _count;
+
+  void increment() {
+    _count++;
+
+    notifyListeners(); // Cela va notifier tous les listeners que quelque chose a changé
+  }
+}
+
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (context) => CounterViewModel(),
+      child: MaterialApp(
+        home: CounterDisplay(),
+      ),
+    );
+  }
+}
+
+class CounterDisplay extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final counterProviderTrue = Provider.of<CounterViewModel>(context,
+        listen: true); // S'abonne aux changements
+
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text(
+          '${counterProviderTrue.count}',
+          style: const TextStyle(fontSize: 24),
+        ),
+        const SizedBox(height: 24),
+        ElevatedButton(
+          onPressed: () {
+            Provider.of<CounterViewModel>(context, listen: false)
+                .increment(); // Ne nécessite pas de reconstruire ce widget
+          },
+          child: const Text('Increment'),
+        ),
+      ],
+    );
+  }
+}
