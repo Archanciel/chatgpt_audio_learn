17bdebdf3853bc9867065ee08d76f96f31191042
Archanciel <jp.schnyder@gmail.com>
2024-05-04 16:15:49 +0200

    works

--- lib/main_help_display.dart
+++ lib/main_help_display.dart
@@ -1,35 +1,37 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Help Display Example'),
-        ),
-        body: Center(
-          child: ElevatedButton(
-            onPressed: () {
-              showDialog(
-                context: context,
-                builder: (context) => HelpDialog(
-                  helpItems: [
-                    HelpItem(
-                        title: 'Default Application',
-                        content:
-                            'If no option is selected, the default playback speed will apply only to newly created playlists.'),
-                    HelpItem(
-                        title: 'Modifying Existing Playlists',
-                        content:
-                            'By selecting this option, all existing playlists will use the new playback speed.'),
-                  ],
-                ),
-              );
-            },
-            child: Text('Show Help'),
+      home: Builder(
+        builder: (context) => Scaffold(
+          body: Center(
+            child: IconButton(
+              icon: const Icon(Icons.help_outline,
+              size: 50.0,),
+              onPressed: () {
+                showDialog(
+                  context: context,
+                  builder: (context) => HelpDialog(
+                    helpItems: [
+                      HelpItem(
+                          titleLocalKey: 'Default Application',
+                          contentLocalKey:
+                              'If no option is selected, the default playback speed will apply only to newly created playlists.'),
+                      HelpItem(
+                          titleLocalKey: 'Modifying Existing Playlists',
+                          contentLocalKey:
+                              'By selecting this option, all existing playlists will use the new playback speed.'),
+                    ],
+                  ),
+                );
+              },
+            ),
           ),
         ),
       ),
@@ -37,20 +39,31 @@ class MyApp extends StatelessWidget {
   }
 }
 
+/// Contains the title local key and content local key of a help item.
+/// 
+/// The local key is the key of the element of the help item defined
+/// in the app_en.arb or app_fr.arb localization files.
 class HelpItem {
-  final String title;
-  final String content;
+  final String titleLocalKey;
+  final String contentLocalKey;
 
-  HelpItem({required this.title, required this.content});
+  HelpItem({
+    required this.titleLocalKey,
+    required this.contentLocalKey,
+  });
 }
 
 class HelpDialog extends StatelessWidget {
   final List<HelpItem> helpItems;
 
-  const HelpDialog({Key? key, required this.helpItems}) : super(key: key);
+  const HelpDialog({
+    Key? key,
+    required this.helpItems,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    int number = 1;
     return Dialog(
       child: Padding(
         padding: const EdgeInsets.all(20.0),
@@ -65,8 +78,10 @@ class HelpDialog extends StatelessWidget {
                   children: [
                     Expanded(
                       child: Text(
-                        item.title,
-                        style: TextStyle(fontWeight: FontWeight.bold),
+                        "${number++}. ${item.titleLocalKey}",
+                        style: const TextStyle(
+                          fontWeight: FontWeight.bold,
+                        ),
                       ),
                     ),
                   ],
@@ -74,12 +89,12 @@ class HelpDialog extends StatelessWidget {
               ),
               Padding(
                 padding: const EdgeInsets.only(bottom: 20),
-                child: Text(item.content),
+                child: Text(item.contentLocalKey),
               ),
             ],
             ElevatedButton(
               onPressed: () => Navigator.of(context).pop(),
-              child: Text('Close'),
+              child: const Text('Close'),
             )
           ],
         ),
